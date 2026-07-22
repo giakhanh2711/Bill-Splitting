52de1f85f84ce146aecc269ec7acc8150e08ba82
Khanh <legiakhanh27@gmail.com>
2026-07-21 19:41:40 -0500

    Initial Commit

---
+++ Bill-Splitting/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Bill-Splitting
+//
+//  Created by Le Gia Khanh on 21/7/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
