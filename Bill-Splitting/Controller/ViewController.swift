d1e75d1d381477297d50abd2bfd1a5546ae57b3f
Khanh <legiakhanh27@gmail.com>
2026-07-31 22:58:44 -0500

    fix ui

--- Bill-Splitting/Controller/ViewController.swift
+++ Bill-Splitting/Controller/ViewController.swift
@@ -9,11 +9,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var totalTipUITextField: UITextField!
+    @IBOutlet weak var numOfPeopleUILabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func percentButtonPressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func uiStepperPressed(_ sender: UIStepper) {
+    }
 }
 
---
+++ Bill-Splitting/Model/TipCalculator.swift
@@ -0,0 +1,7 @@
+//
+//  TipCalculator.swift
+//  Bill-Splitting
+//
+//  Created by Le Gia Khanh on 31/7/26.
+//
+
